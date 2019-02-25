6522546af75a0b0cff2555639c413c63898d6230
Willian <Willian@DESKTOP-B1T9NO5>
2019-02-25 17:13:09 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoBase/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using ProjetoBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoBase.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var aluno = RetornarAlunos().First();
+
+            return View(aluno);
+        }
+        public ActionResult Inserir()
+        {
+            return View();
+
+        }
+        public ActionResult ListarAlunos()
+        {
+            return View(RetornarAlunos().ToList());
+
+        }
+
+        public IQueryable<Aluno> RetornarAlunos()
+        {
+            var alunosLista = new List<Aluno>
+            {
+                new Aluno
+                {
+                    Nome = "teste 01",
+                    Idade = 10
+                },
+                new Aluno
+                {
+                    Nome ="teste 02",
+                    Idade = 11
+                }, new Aluno
+                {
+                    Nome = "teste 03",
+                    Idade = 12
+                }
+            };
+            return alunosLista.AsQueryable();
+
+        }
+
+        [HttpPost]
+        public ActionResult Inserir(Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", aluno);
+            }
+            return View(aluno);
+
+        }
+
+        public ActionResult RelacaoAlunos()
+        {
+
+            return View();
+        }
+
+        public ActionResult RelacaoAlunosAjax()
+        {
+            return View();
+        }
+        public PartialViewResult ListarAlunosAjax()
+        {
+            Thread.Sleep(2000);
+            return PartialView("_ResultadoAlunos", RetornarAlunos().ToList());
+        }
+        
+        public ActionResult InserirAjax()
+        {
+            return View();
+
+        }
+        public PartialViewResult InserirAlunoAjax(Aluno aluno)
+        {
+            Thread.Sleep(2000);
+            return PartialView("_ResultadoInsercaoAjax",aluno);
+        }
+
+
+
+
+    }
+}
---
+++ ProjetoBase/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoBase.Models
+{
+    public class Aluno
+    {
+        [Required(ErrorMessage = "Informe o nome do Aluno!")]
+        [Display(Name = " Nome do Aluno:")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe a idade do Aluno!")]
+        [Range(8, 50, ErrorMessage = "A idade deve estar entre 8 e 50 anos!")]
+        [Display(Name = " Idade do Aluno:")]
+        public int Idade { get; set; }
+
+    }
+}
